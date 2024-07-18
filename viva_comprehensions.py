3726a40444d105117354dcf8ea70bcc6f7933d99
Peter <peter@Zipcoders-MacBook-Pro-6.local>
2024-07-17 20:49:57 -0400

    Tests Pass

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Set, Callable
 import enum
-
+from typing import Callable,Dict #added
+from enum import Enum #added
 
 class Parity(enum.Enum):
     ODD = 0
@@ -9,40 +10,51 @@ class Parity(enum.Enum):
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    Uses a parity based generator to create a lsit 
+    of numbers based on even or odd numbers.
 
     :param start:
     :param stop:
     :param parity:
     :return:
-    """
-    pass
+    """ #updated docstring for clarity
+    
+    # odd
+    if parity == Parity.ODD:
+        #if divisible by 2 (not odd)
+        return [i for i in range(start, stop) if i  % 2 != 0]
+    # even
+    elif parity == Parity.EVEN:
+        return [i for i in range(start, stop) if i % 2 == 0]
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
-
+    Uses a callable (function) to return a dictionary of numbers
+    Keys are integers from start to stop
+    Strategy defines how to calculate the values for each key
+    Anonymous lambda returns square of the number
 
     :param start:
     :param stop:
     :param strategy:
     :return:
     """
+
+    return {i: strategy(i) for i in range(start, stop)}
+
     pass
 
 
-def gen_set(val_in: str) -> Set:
+def gen_set(val_in: str) -> Set[str]:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    Creates a set of uppercase letters of the string's lowercase letters
 
     :param val_in:
     :return:
     """
+
+    return {char.upper() for char in val_in if char.islower()}
+
+
     pass
